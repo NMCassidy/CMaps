bf35b7fc7ebd17ffefdf0e09e4d584dca3632acb
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2016-09-16 14:46:11 +0100

    Added indicator data

--- ShapefilesCode.R
+++ ShapefilesCode.R
@@ -1,5 +1,7 @@
 #------------------Create shapefiles for Cpop maps ---------------
 library(readr)
+library(readxl)
+library(dplyr)
 #read shapefiles
 SpPolysDF<-read_rds("Q:/Codes/Shiny -Leaflet/LeafletPolygons.rds")
 
@@ -12,5 +14,10 @@ SpPolysDF@data <- left_join(SpPolysDF@data, dtaGeoHigher, by = c("group" = "DATA
 
 #If IZs begin with IZ then add Council name to the end
 SpPolysDF@data[grep("IZ",SpPolysDF$INTZONE_NAME), 10] <- paste(SpPolysDF@data[grep("IZ",SpPolysDF$INTZONE_NAME), 10], SpPolysDF@data[grep("IZ",SpPolysDF$INTZONE_NAME), 7], sep = " ")
+
+#Read the data zone indicator data
+indDta <- read_excel("Q:/CMaps/datazone data for maps.xlsx")
+SpPolysDF@data <- left_join(SpPolysDF@data, indDta[c(1,4,5,6,7,8,9)], by = c("DZ_CODE" = "Datazone"))
+  
 #save
 saveRDS(SpPolysDF, file = "Q:/CMaps/Shapes.rds")
